c8be7d2bdb7be9169064e80bbd4374f5a7ee4cb3
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-03-30 16:44:21 +0900

    로그인, 회원가입 화면 구현

--- app/src/main/java/com/example/notiapp/SignInActivity.kt
+++ app/src/main/java/com/example/notiapp/SignInActivity.kt
@@ -2,26 +2,25 @@ package com.example.notiapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class splashActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 2000)
-
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_sign_in)
+
+        findViewById<Button>(R.id.signUpBtn).setOnClickListener {
+            // 회원가입 페이지로 이동
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.SignInmain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/notiapp/SignUpActivity.kt
+++ app/src/main/java/com/example/notiapp/SignUpActivity.kt
@@ -2,26 +2,25 @@ package com.example.notiapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class splashActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up)
 
-        Handler(Looper.getMainLooper()).postDelayed({
+        findViewById<Button>(R.id.signInBtn).setOnClickListener {
+            // 로그인 페이지로 이동
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
-            finish()
-        }, 2000)
+        }
 
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signUpmain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
