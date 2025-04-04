fd05f94b3cc94e5e31d716529ae4184f20156ac7
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-04-04 10:02:07 +0900

    서버 연결 제외 auth 구현

--- android/app/src/main/java/com/example/notiapp/SignInActivity.kt
+++ android/app/src/main/java/com/example/notiapp/SignInActivity.kt
@@ -19,7 +19,15 @@ class SignInActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-        
+
+        // 서버에서 계정 정보 일치 확인 로직
+
+        // 로그인 성공 시 메인 페이지로 이동
+        findViewById<Button>(R.id.signInBtn).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.SignInmain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
